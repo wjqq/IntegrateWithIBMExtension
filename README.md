6328fd7129c0c978767295106dc06e1941266807
jiajwjw-cn <jiajwjw@cn.ibm.com>
2019-12-25 17:50:16 +0800

    v1

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # IntegrateWithIBMExtension
+
+This is hello world work with IBM Blockchain Platform VS Code extension
+
+1 - Install the IBM Blockchain Platform VS Code extension 
+
+https://cloud.ibm.com/docs/services/blockchain/howto?topic=blockchain-develop-vscode
+
+
+2 - Based on the sample chaincode, add one more method history to query the history of user's balance.
+
+func (cc *Chaincode) history(stub shim.ChaincodeStubInterface, args []string) sc.Response..
+
+2.0 First intializate the balance user a and b with 100, 200 seprately.
+
+2.1 invoke method "invoke", transfer 54 from a to b.
+[12/25/2019 5:38:38 PM] [INFO] submitting transaction invoke with args a,b,54 on channel mychannel
+
+2.2 Query user a's balance changes like below.
+[12/25/2019 5:39:01 PM] [SUCCESS] Returned value from history: [{"TxId":"4ce8716a0b851647c42b7aec23fa0e23d3c748be3f9694965900091ba72cc0c9", "Value":100},{"TxId":"9e61e302ecc63646e810c3fa11c95e484877410873780a4226266dae55c129e9", "Value":46}]
+
+2.3 Query user b's balance changes like below.
+[12/25/2019 5:39:19 PM] [SUCCESS] Returned value from history: [{"TxId":"4ce8716a0b851647c42b7aec23fa0e23d3c748be3f9694965900091ba72cc0c9", "Value":200},{"TxId":"9e61e302ecc63646e810c3fa11c95e484877410873780a4226266dae55c129e9", "Value":254}]
+
+
---
+++ chaincode.go
@@ -0,0 +1,237 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+// Chaincode is the definition of the chaincode structure.
+type Chaincode struct {
+}
+
+// Init is called when the chaincode is instantiated by the blockchain network.
+func (cc *Chaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
+
+	fmt.Println("My Init...........................")
+
+	_, args := stub.GetFunctionAndParameters()
+
+	var A, B string    // Entities
+	var Aval, Bval int // Asset holdings
+	var err error
+
+	if len(args) != 4 {
+		return shim.Error("Incorrect number of arguments. Expecting 4")
+	}
+
+	// Initialize the chaincode
+	A = args[0]
+	Aval, err = strconv.Atoi(args[1])
+	if err != nil {
+		return shim.Error("Expecting integer value for asset holding")
+	}
+	B = args[2]
+	Bval, err = strconv.Atoi(args[3])
+	if err != nil {
+		return shim.Error("Expecting integer value for asset holding")
+	}
+	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+
+	// Write the state to the ledger
+	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+// Invoke is called as a result of an application request to run the chaincode.
+func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
+	fmt.Println("ex02 INVOKE...................................")
+	function, args := stub.GetFunctionAndParameters()
+	if function == "invoke" {
+		// Make payment of X units from A to B
+		return cc.invoke(stub, args)
+	} else if function == "delete" {
+		// Deletes an entity from its state
+		return cc.delete(stub, args)
+	} else if function == "query" {
+		// the old "Query" is now implemtned in invoke
+		return cc.query(stub, args)
+	} else if function == "history" {
+		//return all the history of specified user
+		return cc.history(stub, args)
+	}
+
+	return shim.Error("Invalid invoke function name. Expecting \"invoke\" \"Delete\" \"query\"")
+}
+
+// Transaction makes payment of X units from A to B
+func (cc *Chaincode) invoke(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+	var A, B string    // Entities
+	var Aval, Bval int // Asset holdings
+	var X int          // Transaction value
+	var err error
+
+	if len(args) != 3 {
+		return shim.Error("Incorrect number of arguments. Expecting 3")
+	}
+
+	A = args[0]
+	B = args[1]
+
+	// Get the state from the ledger
+	// TODO: will be nice to have a GetAllState call to ledger
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		return shim.Error("Failed to get state")
+	}
+	if Avalbytes == nil {
+		return shim.Error("Entity not found")
+	}
+	Aval, _ = strconv.Atoi(string(Avalbytes))
+
+	Bvalbytes, err := stub.GetState(B)
+	if err != nil {
+		return shim.Error("Failed to get state")
+	}
+	if Bvalbytes == nil {
+		return shim.Error("Entity not found")
+	}
+	Bval, _ = strconv.Atoi(string(Bvalbytes))
+
+	// Perform the execution
+	X, err = strconv.Atoi(args[2])
+	if err != nil {
+		return shim.Error("Invalid transaction amount, expecting a integer value")
+	}
+	Aval = Aval - X
+	Bval = Bval + X
+	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+
+	// Write the state back to the ledger
+	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+// Deletes an entity from state
+func (cc *Chaincode) delete(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	A := args[0]
+
+	// Delete the key from the state in ledger
+	err := stub.DelState(A)
+	if err != nil {
+		return shim.Error("Failed to delete state")
+	}
+
+	return shim.Success(nil)
+}
+
+// query callback representing the query of a chaincode
+func (cc *Chaincode) query(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	var A string // Entities
+	var err error
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
+	}
+
+	A = args[0]
+
+	// Get the state from the ledger
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
+		return shim.Error(jsonResp)
+	}
+
+	if Avalbytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
+		return shim.Error(jsonResp)
+	}
+
+	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+	return shim.Success(Avalbytes)
+}
+
+// query callback representing the query of a chaincode
+func (cc *Chaincode) history(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	var A string // Entities
+	var err error
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
+	}
+
+	A = args[0]
+
+	resultsIterator, err := stub.GetHistoryForKey(A)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing historic values for the marble
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+
+	for resultsIterator.HasNext() {
+		response, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"TxId\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(response.TxId)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Value\":")
+		// if it was a delete operation on given key, then we need to set the
+		//corresponding value null. Else, we will write the response.Value
+		//as-is (as the Value itself a JSON marble)
+		buffer.WriteString(string(response.Value))
+
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+
+	buffer.WriteString("]")
+
+	return shim.Success(buffer.Bytes())
+}
